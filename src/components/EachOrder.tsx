6989fba2b748a9a7842a42db28957be8103ebb87
dong1hyun <limd1238@naver.com>
2024-09-18 19:17:50 +0900

    네이버맵 api 연동

--- src/components/EachOrder.tsx
+++ src/components/EachOrder.tsx
@@ -1,4 +1,4 @@
-import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
+import { Alert, Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
 import orderSlice, { Order } from "../slices/order";
 import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,12 +8,13 @@ import Config from "react-native-config";
 import { RootState } from "../store/reducer";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { LoggedInParamList } from "../../AppInner";
-import EncryptedStorage from "react-native-encrypted-storage";
-import userSlice from "../slices/user";
+import NaverMapView, { Marker, Path } from "react-native-nmap";
 
 function EachOrder({ item }: { item: Order }) {
+    console.log('start:', item.start, 'end:', item.end);
     const [detail, setDetail] = useState(false);
     const [loading, setLoading] = useState(false);
+    const { start, end } = item;
     const accessToken = useSelector<RootState>(state => state.user.accessToken);
     const dispatch = useAppDispatch();
     const navigation = useNavigation<NavigationProp<LoggedInParamList>>();
@@ -37,9 +38,6 @@ function EachOrder({ item }: { item: Order }) {
                 Alert.alert('알림', errorResponse.data.message);
                 dispatch(orderSlice.actions.rejectOrder(item.orderId));
             }
-            if (errorResponse?.status === 419) {
-
-            }
             setLoading(false);
         }
     }, []);
@@ -58,8 +56,41 @@ function EachOrder({ item }: { item: Order }) {
             </Pressable>
             {detail ?
                 (<View>
-                    <View>
-                        <Text>네이버맵</Text>
+                    <View
+                        style={{
+                            width: Dimensions.get('window').width - 30,
+                            height: 200,
+                            marginTop: 10,
+                        }}>
+                        <NaverMapView
+                            style={{ width: '100%', height: '100%' }}
+                            zoomControl={false}
+                            center={{
+                                zoom: 10,
+                                tilt: 50,
+                                latitude: (start.latitude + end.latitude) / 2,
+                                longitude: (start.longitude + end.longitude) / 2,
+                            }}>
+                            <Marker
+                                coordinate={{
+                                    latitude: start.latitude,
+                                    longitude: start.longitude,
+                                }}
+                                pinColor="blue"
+                            />
+                            <Path
+                                coordinates={[
+                                    {
+                                        latitude: start.latitude,
+                                        longitude: start.longitude,
+                                    },
+                                    { latitude: end.latitude, longitude: end.longitude },
+                                ]}
+                            />
+                            <Marker
+                                coordinate={{ latitude: end.latitude, longitude: end.longitude }}
+                            />
+                        </NaverMapView>
                     </View>
                     <View style={styles.bttonWrapper}>
                         <Pressable onPress={onAccept} disabled={loading} style={styles.acceptButton}>
--- src/slices/order.ts
+++ src/slices/order.ts
@@ -23,96 +23,96 @@ const initialState: InitialState = {
         {
             orderId: 'ukKUKniYP',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 3000,
         },
         {
             orderId: '9ZLbPkiXj',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 3400,
         },
         {
             orderId: 'kq2Ymi3Xi',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 2800,
         },
         {
             orderId: 'ieUhPYGt',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 2800,
         },
         {
             orderId: '5',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 2800,
         },
         {
             orderId: '6',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 2800,
         },
         {
             orderId: '7',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 2800,
         },
         {
             orderId: '8',
             start: {
-                latitude: 127.3,
-                longitude: 35,
+                latitude: 37.552920604789094,
+                longitude: 126.97274877551573,
             },
             end: {
-                latitude: 127.4,
-                longitude: 35.1,
+                latitude: 37.535621724562,
+                longitude: 126.97411279225547,
             },
             price: 2800,
         }
